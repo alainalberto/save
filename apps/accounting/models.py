637381d19f03d3774620fdd90e5af3e099e65965
Alain <alainalberto03@gmail.com>
2017-06-10 14:17:17 -0500

    Alain: Se agrego la columna list_item en el modelo

--- apps/accounting/models.py
+++ apps/accounting/models.py
@@ -87,6 +87,7 @@ class Item(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
     description = models.CharField(max_length=255, blank=True, null=True)
     value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    list_item = models.BooleanField(default=True)
 
     def __str__(self):
         return '{}'.format(self.name)
